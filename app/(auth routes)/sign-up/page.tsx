2c2d9621466810a75ff818444aa02dc77f5cbe94
nacchan-kun <sendono.nc17@gmail.com>
2025-07-22 21:05:00 +0300

    nyt

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -1,58 +1,58 @@
 'use client';
 
-import { RegisterRequest } from '@/types/authorisationTypes';
-import css from './SignUpPage.module.css';
-import { useRouter } from 'next/navigation';
 import { register } from '@/app/api/clientApi';
-import { useState } from 'react';
+import { RegisterRequest } from '@/types/authorisationTypes';
 import { useAuthStore } from '@/lib/store/authStore';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-export default function Register() {
+import css from './SignUpPage.module.css';
+
+export default function SignUp() {
   const router = useRouter();
   const [error, setError] = useState('');
+
   const setUser = useAuthStore(state => state.setUser);
 
-  async function handleRegister(formData: FormData) {
+  const handleSubmit = async (formData: FormData) => {
     try {
-      setError('');
       const formValues = Object.fromEntries(formData) as RegisterRequest;
-      const res = await register(formValues);
-      if (res) {
-        // Записуємо користувача у глобальний стан
-        setUser(res);
-        router.push('/profile');
+      const user = await register(formValues);
+      if (user) {
+        setUser(user);
+        router.replace('/profile');
       } else {
         setError('Invalid email or password');
       }
     } catch (error) {
       console.log('error', error);
-      setError('Invalid email or password');
+      setError('Oops... Something went wrong, try later');
     }
-  }
+  };
 
   return (
     <>
       <h1 className={css.formTitle}>Sign up</h1>
-      <form action={handleRegister} className={css.form}>
+      <form action={handleSubmit} className={css.form}>
         <div className={css.formGroup}>
           <label htmlFor="email">Email</label>
           <input
-            id="email"
             type="email"
             name="email"
-            className={css.input}
+            id="email"
             required
+            className={css.input}
           />
         </div>
 
         <div className={css.formGroup}>
           <label htmlFor="password">Password</label>
           <input
-            id="password"
             type="password"
             name="password"
-            className={css.input}
+            id="password"
             required
+            className={css.input}
           />
         </div>
 
@@ -61,9 +61,8 @@ export default function Register() {
             Register
           </button>
         </div>
-
-        <p className={css.error}>{error}</p>
       </form>
+      {error && <p>{error}</p>}
     </>
   );
 }

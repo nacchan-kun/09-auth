3004d5c926335c49576e26dc11926e16f89672f6
nacchan-kun <sendono.nc17@gmail.com>
2025-07-23 23:30:34 +0300

    ghcf

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,24 +1,9 @@
-// app/(public routes)/layout.tsx
-
-'use client';
-
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+// app/(auth routes)/layout.tsx
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function PublicLayout({ children }: Props) {
-  const [loading, setLoading] = useState(true);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    // refresh викличе перезавантаження даних
-    router.refresh();
-    setLoading(false);
-  }, [router]);
-
-  return <>{loading ? <div>Loading...</div> : children}</>;
+export default function AuthLayout({ children }: Props) {
+  return <>{children}</>;
 }

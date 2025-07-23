ead03df02920aabca0e8ff79e1526e194d071251
nacchan-kun <sendono.nc17@gmail.com>
2025-07-23 22:40:02 +0300

    Update default.tsx

---
+++ app/(private routes)/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,33 @@
+import styles from './SidebarNotes.module.css';
+import { fetchNotes } from '@/lib/api/serverApi';
+import Link from 'next/link';
+import type { Tag } from '@/types/note';
+
+const NotesSidebar = async () => {
+  const response = await fetchNotes({});
+
+  const rawTags: Tag[] = response?.notes?.map(note => note.tag) ?? [];
+  const uniqueTags = Array.from(new Set<Tag>(rawTags));
+  const tags: Tag[] = [...uniqueTags];
+
+  return (
+    <aside>
+      <ul className={styles.menuList}>
+        <li className={styles.menuItem}>
+          <Link href="/notes/filter/all" className={styles.menuLink}>
+            All notes
+          </Link>
+        </li>
+        {tags.map(tag => (
+          <li key={tag} className={styles.menuItem}>
+            <Link href={`/notes/filter/${tag}`} className={styles.menuLink}>
+              {tag}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default NotesSidebar;

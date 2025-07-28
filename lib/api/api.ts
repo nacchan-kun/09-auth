0c22cef9f7ed4fdfc14dbfe43108b9da711b6734
nacchan-kun <sendono.nc17@gmail.com>
2025-07-28 21:31:07 +0300

    Update api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://notehub-api.goit.study';
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 export const api = axios.create({
   baseURL,
